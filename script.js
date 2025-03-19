e2f1ba3635f1f32f99e5f97dcc7aa735c1a9f93b
VyasAJ7 <vyasaj7@gmail.com>
2025-03-20 00:08:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function checkWhitelist() {
 
     document.getElementById("loading").style.display = "block";
 
-    fetch("https://script.google.com/macros/s/AKfycbyTNBGpnGRVJilWj9e9akOht182D6F8M_Ra2PmKsRbOTgjS32Z5ZJi1MSQlGrAlsjVy/exec", {
+    fetch("https://script.google.com/macros/s/AKfycby_AMuTcGGyOPBRe6qsIyPXAxr4Geq8oJmUXwnhBG2o2bo8e_Hdy7EZ3HJUQ7HNw5F0/exec", {
         method: "POST",
         body: JSON.stringify({ userWhitelist }),
         headers: {
